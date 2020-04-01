3a9a95c79a0cd27764bd015ac4f48677da88c66a
Daniel Bogorad <dsbogorad@gmail.com>
2020-04-01 20:09:58 +0300

    minor cleanups

--- get_korea_cdc_data.py
+++ get_korea_cdc_data.py
@@ -2,27 +2,27 @@ import os
 import re
 import timeit
 import asyncio
-import urllib.request
 from datetime import datetime
 import aiohttp
 
 from bs4 import BeautifulSoup as BS
 
+
 BASE_URL = "https://www.cdc.go.kr"
 
 
-# TODO: this function should probably be an async generator.
-async def get_all_report_links_(session, base_url):
+async def get_all_report_links(session, base_url):
+    # TODO: this function should probably be an async generator.
+
     current_page = None
     report_links = []
     oldest_report_number = 'list_no=365797'
+    first_page_url = "/board/board.es?mid=&bid=0030&nPage=1"
 
-    async with session.get(f"{base_url}/board/board.es?mid=&bid=0030&nPage=1") as resp:
+    async with session.get(f"{base_url}{first_page_url}") as resp:
         html = await resp.text()
         current_page = BS(html, features='lxml')
 
-    # TODO: this value will grow as more reports are added. Change this to dynamically
-    # find the oldest relevant page.
     while True:
         current_page_report_links = [
             link.get('href') for link in current_page.find_all('a')]
@@ -32,7 +32,7 @@ async def get_all_report_links_(session, base_url):
             if 'list_no=' in report_link:
                 report_links.append(report_link)
 
-            # TODO: this condition will probably be a bug when this function will be truly async.
+            # TODO: this condition will probably be a bug when this function is truly async.
             if oldest_report_number in report_link:
                 # reached the oldest relevant report (2020-01-22)
                 return report_links
@@ -45,64 +45,40 @@ async def get_all_report_links_(session, base_url):
     return report_links
 
 
-def get_all_report_links(base_url):
-    site = urllib.request.urlopen(f"{base_url}/board/board.es?mid=&bid=0030&nPage=1")
-    site_soup = BS(site, features='lxml')
-    report_links = []
-    next_page = site_soup
-    current_page = 0
-    # TODO: this value will grow as more reports are added. Change this to dynamically find the oldest relevant page.
-    while current_page != 15:
-        print(f"gathered {len(report_links)}")
-        links = [link.get('href') for link in next_page.find_all('a')]
-        for l in links:
-            if 'rss' in l:
-                continue
-            if 'list_no=' in l:
-                report_links.append(l)
-        next_page_link = next_page.find_all(
-            'a', class_='pageNext')[0].get('href')
-        next_page = BS(urllib.request.urlopen(
-            f'{base_url}{next_page_link}'), features='lxml')
-        current_page += 1
-    # global glob_report_links = report_links
+def save_report_to_file(path, filename, report):
+    with open(os.path.join(path, filename), "w+") as report_file:
+        report_file.write(report)
 
 
-async def get_single_report(base_url, i, link, session, total_links_amount):
-    print(f"({datetime.now()} - getting report {i+1} of {total_links_amount}: {link}")
+async def get_single_report(base_url, i, report_link, session, total_links_amount):
+    print(f"({datetime.now()} - getting report {i+1} of {total_links_amount}: {report_link}")
 
     report_text = None
-    async with session.get(f"{base_url}{link}") as resp:
+    async with session.get(f"{base_url}{report_link}") as resp:
         report_html = await resp.text()
         report_text = BS(report_html, features='lxml').get_text()
 
         report_date = re.search(r'Date\d{4}-\d{2}-\d{2}', report_text)
         date_filename = str(i) if not report_date else f"{report_date.group()[4:]}_{i}"
-
-        if '2018' in date_filename or '2019' in date_filename:
-            # A somewhat ugly way to detect when we went beyond the oldest relevant review
-            return
-
-        with open(os.path.join("reps", date_filename), "w+") as report_file:
-            written = report_file.write(report_text)
+        save_report_to_file("Other_data/south-korea-cdc-reports", date_filename, report_text)
 
 
 async def main():
-    print(f"starting to get all the linked reports ({datetime.now()})")
+    print(f"Getting all the reports, starting now. ({datetime.now()})")
 
     async with aiohttp.ClientSession() as session:
         start_time = timeit.default_timer()
 
-        glob_report_links = await get_all_report_links_(session, BASE_URL)
-        total_links_amount = len(glob_report_links)
+        all_report_links = await get_all_report_links(session, BASE_URL)
+        total_links_amount = len(all_report_links)
+
         coros = []
-        for i, link in enumerate(glob_report_links):
+        for i, link in enumerate(all_report_links):
             coros.append(get_single_report(BASE_URL, i, link, session, total_links_amount))
-        print(len(coros))
         await asyncio.gather(*coros)
 
         end_time = timeit.default_timer()
-        print(f"finished downloading {total_links_amount} which took {end_time - start_time} sec")
+        print(f"finished downloading {total_links_amount} reports which took {end_time - start_time} sec")
 
 
 if __name__ == '__main__':
