fd9dbb94e468c6323d5c9383fda987b7cb1847f0
Daniel Bogorad <dsbogorad@gmail.com>
2020-04-08 21:13:12 +0300

    some cleanups

--- get_korea_cdc_data.py
+++ get_korea_cdc_data.py
@@ -21,8 +21,6 @@ from bs4 import BeautifulSoup as BS
 BASE_URL = "https://www.cdc.go.kr"
 REPORT_DIR = "Other_data/south-korea-cdc-reports"
 
-REPORT_QUEUE = None
-DONE_FETCHING_LINKS_EVENT = None
 
 async def get_all_report_links(session, base_url, queue):
     current_page = None
@@ -83,6 +81,7 @@ def extract_statistic_data(report_html, report_date, date_filename):
 
     pass
 
+
 async def get_single_rep(base_url, report_link, session, queue, i):
     print(f"({datetime.now()} - getting report {i+1}: {report_link}")
     async with session.get(f"{base_url}{report_link}") as resp:
@@ -98,7 +97,6 @@ async def get_single_rep(base_url, report_link, session, queue, i):
 
 
 async def get_reports(base_url, session, queue):
-    report_text = None
     i = 0
 
     while True:
@@ -109,28 +107,13 @@ async def get_reports(base_url, session, queue):
         i += 1
 
 
-async def get_single_report(base_url, i, report_link, session, total_links_amount):
-    print(f"({datetime.now()} - getting report {i+1} of {total_links_amount}: {report_link}")
-
-    report_text = None
-    async with session.get(f"{base_url}{report_link}") as resp:
-        report_html = await resp.text()
-        report_text = BS(report_html, features='lxml').get_text()
-
-        report_date = re.search(r'Date\d{4}-\d{2}-\d{2}', report_text)
-        date_filename = str(i) if not report_date else f"{report_date.group()[4:]}_{i}"
-        save_report_to_file(os.path.join(REPORT_DIR, 'text'), date_filename, report_text)
-        extract_statistic_data(report_html, report_date, date_filename)
-
 def save_test_data_to_csv(data_row: BS, report_date):
-    TESTED_NOW_COL_INDEX = 2
-    TESTED_NEGATIVE_COL_INDEX = 3
+    tested_no_col_index = 2
+    tested_negative_col_index = 3
     cols = data_row.find_all('td')
-    raw_value = lambda val: val.get_text().replace(',','').replace('.','')
-    # tested_now = int(cols[len(cols)-TESTED_NOW_COL_INDEX].get_text().replace(',', '').replace('.', ''))
-    tested_now = int(raw_value(cols[len(cols)-TESTED_NOW_COL_INDEX]))
-    # tested_negative = int(cols[len(cols)-TESTED_NEGATIVE_COL_INDEX].get_text().replace(',', '').replace('.', ''))
-    tested_negative = int(raw_value(cols[len(cols)-TESTED_NEGATIVE_COL_INDEX]))
+    raw_value = lambda val: val.get_text().replace(',', '').replace('.', '')
+    tested_now = int(raw_value(cols[len(cols)-tested_no_col_index]))
+    tested_negative = int(raw_value(cols[len(cols)-tested_negative_col_index]))
     total_tested = tested_now + tested_negative
 
     with open(os.path.join(REPORT_DIR, 'csv', f'{report_date}.csv'), 'w', newline='') as csv_file:
@@ -138,7 +121,7 @@ def save_test_data_to_csv(data_row: BS, report_date):
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerow({'date': report_date, 'under_examination': tested_now,
-                            'negative': tested_negative, 'total_tested': total_tested})
+                         'negative': tested_negative, 'total_tested': total_tested})
         print(f"tested now: {tested_now}, negative: {tested_negative}")
 
 
@@ -150,8 +133,6 @@ async def main2():
     unexpected_tables = 0
 
     scan_dir = os.path.join(REPORT_DIR, "html")
-    # not bad. 160 pages had the table out of 176 pages.
-    #
     filenames = os.listdir(scan_dir)
     filenames.sort()
 
@@ -162,7 +143,7 @@ async def main2():
         report_soup = None
         with open(os.path.join(scan_dir, report), 'r') as report_file:
             report_soup = BS(report_file.read(), features='lxml')
-        
+
         # find out how many pages have the first table
         table = report_soup.find('table')
         if table is not None:
@@ -182,12 +163,12 @@ async def main2():
 
             save_test_data_to_csv(current_test_row, report_date)
 
-                # for row in table.find_all('tr'):
-                #     first_col = row.find('td')
-                #     if first_col is not None:
-                #         first_col_text = first_col.get_text()
-                #         if "as of" in first_col_text.lower():
-                #             as_of_cols += 1
+            # for row in table.find_all('tr'):
+            #     first_col = row.find('td')
+            #     if first_col is not None:
+            #         first_col_text = first_col.get_text()
+            #         if "as of" in first_col_text.lower():
+            #             as_of_cols += 1
 
     print(f"scanned {pages_scanned} pages and found {tables} first tables.")
     print(
@@ -197,34 +178,22 @@ async def main2():
 
 async def main():
     print(f"Getting all the reports, starting now. ({datetime.now()})")
-    REPORT_QUEUE = asyncio.Queue()
-    REPORT_QUEUE.put_nowait(None)
+    report_queue = asyncio.Queue()
+    report_queue.put_nowait(None)
 
     async with aiohttp.ClientSession() as session:
         start_time = timeit.default_timer()
-
-        # all_report_links = await get_all_report_links(session, BASE_URL)
-        # total_links_amount = len(all_report_links)
-
-
         coros = [
-            asyncio.create_task(get_reports(BASE_URL, session, REPORT_QUEUE)),
-            asyncio.create_task(get_all_report_links(session, BASE_URL, REPORT_QUEUE)),
-            # get_reports(BASE_URL, session),
-            # get_all_report_links(session, BASE_URL),
+            asyncio.create_task(get_reports(BASE_URL, session, report_queue)),
+            asyncio.create_task(get_all_report_links(session, BASE_URL, report_queue)),
         ]
 
-        # await DONE_FETCHING_LINKS_EVENT.wait()
-        await REPORT_QUEUE.join()
+        await report_queue.join()
         [coro.cancel() for coro in coros]
-        # for i, link in enumerate(all_report_links):
-        #     coros.append(get_single_report(BASE_URL, i, link, session, total_links_amount))
-            # await get_single_report(BASE_URL, i, link, session, total_links_amount)
         await asyncio.gather(*coros, return_exceptions=True)
 
         end_time = timeit.default_timer()
-        # print( f"finished downloading {total_links_amount} reports which took {end_time - start_time} sec")
-        print( f"finished downloading. took {end_time - start_time} sec")
+        print(f"finished downloading. took {end_time - start_time} sec")
 
 
 if __name__ == '__main__':
