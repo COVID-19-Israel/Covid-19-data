b5d0a7d479f5c97a5999bc01b272a069d9e86ce3
Daniel Bogorad <18028607+dbogo@users.noreply.github.com>
2020-04-16 08:13:59 +0300

    wip on korean tests csv fix

--- src/koreaCdc/get_korea_cdc_data.py
+++ src/koreaCdc/get_korea_cdc_data.py
@@ -64,6 +64,45 @@ def save_report_to_file(path, filename, report):
         report_file.write(report)
 
 
+def save_test_data_to_csv(data_row: BS, report_date):
+    tested_no_col_index = 2
+    tested_negative_col_index = 3
+    cols = data_row.find_all('td')
+    def raw_value(val): return val.get_text().replace(',', '').replace('.', '')
+    tested_now = int(raw_value(cols[len(cols)-tested_no_col_index]))
+    tested_negative = int(raw_value(cols[len(cols)-tested_negative_col_index]))
+    total_tested = tested_now + tested_negative
+
+    with open(os.path.join(BASE_OUTPUT_PATH, 'csv', f'{report_date}.csv'), 'w', newline='') as csv_file:
+        fieldnames = ['date', 'under_examination', 'negative', 'total_tested']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'date': report_date, 'under_examination': tested_now,
+                         'negative': tested_negative, 'total_tested': total_tested})
+        print(f"tested now: {tested_now}, negative: {tested_negative}")
+
+
+def get_first_table_data(report_soup, report_date):
+    table = report_soup.find('table')
+    if table is not None:
+
+        # find how many of those tables have the "As of X" row
+        rows = table.find_all('tr')
+        current_test_row = None
+        if len(rows) == 3:
+            current_test_row = rows[2]
+        elif len(rows) == 5:
+            current_test_row = rows[3]
+        else:
+            # print(f"found a table with unexpected number of rows in {report}")
+            print(f"found a table with unexpected number of rows in {report_date}")
+            return
+            # unexpected_tables += 1
+            # continue
+
+        save_test_data_to_csv(current_test_row, report_date)
+
+
 def extract_statistic_data(report_html, report_date, date_filename):
     """
     Get the following data from the report:
@@ -83,15 +122,18 @@ def extract_statistic_data(report_html, report_date, date_filename):
 
 
 async def get_single_rep(base_url, report_link, session, queue, i):
-    print(f"({datetime.now()} - getting report {i+1}: {report_link}")
+    print(f'({datetime.now()} - getting report {i+1}: {report_link}')
     async with session.get(f"{base_url}{report_link}") as resp:
         report_html = await resp.text()
-        report_text = BS(report_html, features='lxml').get_text()
+        report = BS(report_html, features='lxml')
+        report_text = report.get_text()
 
         report_date = re.search(r'Date\d{4}-\d{2}-\d{2}', report_text)
-        date_filename = str(i) if not report_date else f"{report_date.group()[4:]}_{i}"
+        date_filename = str(i) if not report_date else f'{report_date.group()[4:]}_{i}'
         save_report_to_file(os.path.join(BASE_OUTPUT_PATH, 'text'), date_filename, report_text)
-        save_report_to_file(os.path.join(BASE_OUTPUT_PATH, "html"), f"{date_filename}.html", report_html)
+        save_report_to_file(os.path.join(BASE_OUTPUT_PATH, 'html'),
+                            f'{date_filename}.html', report_html)
+        get_first_table_data(report, date_filename[:10])
         # extract_statistic_data(report_html, report_date, date_filename)
         queue.task_done()
 
@@ -106,41 +148,6 @@ async def get_reports(base_url, session, queue):
         asyncio.create_task(get_single_rep(base_url, report_link, session, queue, i))
         i += 1
 
-
-def save_test_data_to_csv(data_row: BS, report_date):
-    tested_no_col_index = 2
-    tested_negative_col_index = 3
-    cols = data_row.find_all('td')
-    raw_value = lambda val: val.get_text().replace(',', '').replace('.', '')
-    tested_now = int(raw_value(cols[len(cols)-tested_no_col_index]))
-    tested_negative = int(raw_value(cols[len(cols)-tested_negative_col_index]))
-    total_tested = tested_now + tested_negative
-
-    with open(os.path.join(BASE_OUTPUT_PATH, 'csv', f'{report_date}.csv'), 'w', newline='') as csv_file:
-        fieldnames = ['date', 'under_examination', 'negative', 'total_tested']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerow({'date': report_date, 'under_examination': tested_now,
-                         'negative': tested_negative, 'total_tested': total_tested})
-        print(f"tested now: {tested_now}, negative: {tested_negative}")
-
-def get_first_table_data(report_soup, report_date):
-    table = report_soup.find('table')
-    if table is not None:
-
-        # find how many of those tables have the "As of X" row
-        rows = table.find_all('tr')
-        current_test_row = None
-        if len(rows) == 3:
-            current_test_row = rows[2]
-        elif len(rows) == 5:
-            current_test_row = rows[3]
-        else:
-            print(f"found a table with unexpected number of rows in {report}")
-            # unexpected_tables += 1
-            # continue
-
-        save_test_data_to_csv(current_test_row, report_date)
 
 async def main2():
 
@@ -162,25 +169,26 @@ async def main2():
             report_soup = BS(report_file.read(), features='lxml')
 
         # find out how many pages have the first table
-        get_first_table_data(report_soup, report_date)		
+        get_first_table_data(report_soup, report_date)
 
-            # for row in table.find_all('tr'):
-            #     first_col = row.find('td')
-            #     if first_col is not None:
-            #         first_col_text = first_col.get_text()
-            #         if "as of" in first_col_text.lower():
-            #             as_of_cols += 1
+        # for row in table.find_all('tr'):
+        #     first_col = row.find('td')
+        #     if first_col is not None:
+        #         first_col_text = first_col.get_text()
+        #         if "as of" in first_col_text.lower():
+        #             as_of_cols += 1
 
     print(f"scanned {pages_scanned} pages and found {tables} first tables.")
     print(
         f'out of those {tables} tables, {unexpected_tables} unexpected tables were found that were not parsed.')
     # print(f'found {as_of_cols} "as of" columns in {tables} tables')
 
+
 def create_output_dirs():
     os.makedirs(os.path.join(BASE_OUTPUT_PATH, "text"), mode=0o755, exist_ok=True)
     os.makedirs(os.path.join(BASE_OUTPUT_PATH, "csv"), mode=0o755, exist_ok=True)
     os.makedirs(os.path.join(BASE_OUTPUT_PATH, "html"), mode=0o755, exist_ok=True)
-    
+
 
 async def main():
     print(f"Getting all the reports, starting now. ({datetime.now()})")
